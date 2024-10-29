3a17545944331b5683d96d0b532d540d6cbe1d55
alig329 <muhali2333@gmail.com>
2024-10-29 14:30:51 +0500

    added post routes

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,19 @@ app.get('/', (req, res) => {
   res.send('Hello, Ali! Your server is running.');
 });
 
+app.post('/auth', (req, res) => {const{name, email, password}=req.body;
+RegisterModel.findOne({email: email})
+.then(user=> {
+  if(user){
+    res.json("Account already exits")
+  } else{
+    RegisterModel.create({name: name, email: email, password: password})
+    .then(result=> res.json(result))
+    .catch(err => res.json(err))
+  }
+}).catch(err => res.json(err))
+})
+
 const PORT = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
